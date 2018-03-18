742b146202b24838ab1e22c90b65331ef58f5952
newtonis <ariel.nowik@gmail.com>
2018-03-18 02:58:03 -0300

    agregando I

---
+++ ejI/ejI.m
@@ -0,0 +1,18 @@
+
+A=imread('lena512.bmp');
+
+B = reshape(A,[],1);
+
+mu = median(B);
+s = std(double(B));
+ 
+arr = [0.5,1,2];
+for ri=1:length(arr)
+ r = arr(ri);
+ info = func_data(mu - r * s , mu + r * s,A);
+ 
+ imwrite( info, strjoin({'output_',int2str(ri),'.bmp'}));
+ 
+end
+ 
+ 
---
+++ ejI/func_data.m
@@ -0,0 +1,11 @@
+function [out ] = func_data( a,b , A)
+    m = min(min(A));
+    M = max(max(A));
+    a = max(a,m);
+    b = min(b,M);
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    out = (double(a) + double(b-a)/double(M-m)) * (A - m);
+   
+end
+
