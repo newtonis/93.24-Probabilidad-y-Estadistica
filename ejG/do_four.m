de1ff9b54737ae21521006b660c377a0abad8cc0
newtonis <ariel.nowik@gmail.com>
2018-04-01 15:23:01 -0300

    updated doc

--- ejG/do_four.m
+++ ejG/do_four.m
@@ -3,7 +3,7 @@ function [ ] = do_four( N )
 %   Detailed explanation goes here
 
     
-    cut16('lena512.bmp',strjoin({'data/min_output_',num2str(N),'.bmp'}),N,@min);
+    cut16('lena512.bmp',D({'data/min_output_',num2str(N),'.bmp'}),N,@min);
     cut16('lena512.bmp',strjoin({'data/mean_output_',num2str(N),'.bmp'}),N,@mean);
     cut16('lena512.bmp',strjoin({'data/median_output',num2str(N),'.bmp'}),N,@median);
     cut16('lena512.bmp',strjoin({'data/median_max',num2str(N),'.bmp'}),N,@max);
